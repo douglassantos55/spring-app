b6fc2fc2e5738bab3c91cb8dfc360519aaaf8094
Douglas Santos <hiperhavy_doug@hotmail.com>
2023-01-28 16:27:17 -0300

    setup

---
+++ src/main/java/br/com/fastfood/restaurant/RestaurantApplication.java
@@ -0,0 +1,20 @@
+package br.com.fastfood.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class RestaurantApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello, World!";
+	}
+}
