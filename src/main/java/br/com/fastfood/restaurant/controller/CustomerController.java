601ba006df46205371617f2e9caeed694542c78e
Douglas Santos <hiperhavy_doug@hotmail.com>
2023-02-07 21:52:11 -0300

    feat: list customers

--- src/main/java/br/com/fastfood/restaurant/controller/CustomerController.java
+++ src/main/java/br/com/fastfood/restaurant/controller/CustomerController.java
@@ -3,11 +3,11 @@ package br.com.fastfood.restaurant.controller;
 import br.com.fastfood.restaurant.entity.Customer;
 import br.com.fastfood.restaurant.repository.CustomerRepository;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/customers")
@@ -22,5 +22,13 @@ public class CustomerController {
     @PostMapping
     public Customer post(@RequestBody @Valid Customer customer) {
         return this.repository.save(customer);
+    }
+
+    @GetMapping
+    public Page<Customer> list(
+            @Min(1) @RequestParam(value = "page", defaultValue = "1") int page,
+            @Min(1) @RequestParam(value = "per_page", defaultValue = "50") int perPage
+    ) {
+        return this.repository.findAll(PageRequest.of(page-1, perPage));
     }
 }
